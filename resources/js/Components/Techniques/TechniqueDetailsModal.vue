2a05deb0852c840c50baaf8706e6ce1522c7847e
Delia Paternina <delia@Delias-Air.broadband>
2025-01-08 18:23:07 +0000

    adds gear details

--- resources/js/Components/Techniques/TechniqueDetailsModal.vue
+++ resources/js/Components/Techniques/TechniqueDetailsModal.vue
@@ -1,6 +1,6 @@
 <template>
     <v-card>
-        <TechniqueCard 
+        <TechniqueCard
             :technique="selectedTechnique"
             class="ma-4"
             @selected="closeModal"
@@ -8,14 +8,35 @@
         <v-card-subtitle>Steps to practice</v-card-subtitle>
         <v-card-text>{{ technique.steps_to_practice }}</v-card-text>
         <v-card-text>
-                <strong>Gear:</strong>
-                <ul v-if="technique.gear && technique.gear.length">
-                    <li v-for="item in technique.gear" :key="item.id">
-                        {{ item.name }}
-                    </li>
-                </ul>
-                <p v-else>No gear associated with this technique.</p>
-            </v-card-text>
+            <strong>Gear:</strong>
+
+            <ul v-if="technique.gear && technique.gear.length">
+                <li
+                    v-for="item in technique.gear"
+                    :key="item.id"
+                    @click="openGearDetails(item)"
+                    class="gear-item text-primary"
+                >
+                    {{ item.name }}
+                </li>
+            </ul>
+            <p v-else>No gear associated with this technique.</p>
+        </v-card-text>
+        <v-dialog v-model="gearDialogVisible" max-width="400px">
+            <v-card v-if="selectedGear">
+                <v-card-title>{{ selectedGear.name }}</v-card-title>
+                <v-card-subtitle
+                    >Category: {{ selectedGear.category }}</v-card-subtitle
+                >
+                <v-card-text
+                    >Description: {{ selectedGear.description }}</v-card-text
+                >
+                <v-card-actions>
+                    <v-btn @click="closeGearDetails">Close</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
         <v-card-actions>
             <v-btn @click="closeModal">Close</v-btn>
         </v-card-actions>
@@ -23,7 +44,8 @@
 </template>
 
 <script>
-import TechniqueCard from './TechniqueCard.vue';
+import TechniqueCard from "./TechniqueCard.vue";
+import "/resources/css/components/_modal.css";
 
 export default {
     props: {
@@ -33,17 +55,31 @@ export default {
         },
     },
     components: {
-        TechniqueCard
+        TechniqueCard,
+    },
+    data() {
+        return {
+            gearDialogVisible: false,
+            selectedGear: null,
+        };
     },
     computed: {
         selectedTechnique() {
-            return this.$store.getters['techniques/selectedTechnique']
-        }
+            return this.$store.getters["techniques/selectedTechnique"];
+        },
     },
     methods: {
         closeModal() {
             this.$emit("close");
-        }
+        },
+        openGearDetails(gear) {
+            this.selectedGear = gear;
+            this.gearDialogVisible = true;
+        },
+        closeGearDetails() {
+            this.gearDialogVisible = false;
+            this.selectedGear = null;
+        },
     },
 };
 </script>
